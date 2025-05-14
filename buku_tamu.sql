853b31deddcb8101c523a1334d4abc04cfe98782
Ariel Gema Wardana <2024.arielgemaw.rplb@student.unsia.ac.id>
2025-05-14 15:33:39 +0700

    docs: adding database

---
+++ buku_tamu.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: May 14, 2025 at 08:33 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.3.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_bukutamu`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buku_tamu`
+--
+
+CREATE TABLE `buku_tamu` (
+  `id` int NOT NULL,
+  `nama` varchar(225) NOT NULL,
+  `instansi` varchar(225) NOT NULL,
+  `tujuan` text NOT NULL,
+  `tanggal` date NOT NULL,
+  `waktu` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `buku_tamu`
+--
+
+INSERT INTO `buku_tamu` (`id`, `nama`, `instansi`, `tujuan`, `tanggal`, `waktu`) VALUES
+(7, 'Mark Zuckerberg', 'Meta', 'Jakarta, Indonesia', '2025-05-14', '12:28:40'),
+(10, 'test tambah', 'test', 'test', '2025-05-14', '12:31:04');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `buku_tamu`
+--
+ALTER TABLE `buku_tamu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `buku_tamu`
+--
+ALTER TABLE `buku_tamu`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
